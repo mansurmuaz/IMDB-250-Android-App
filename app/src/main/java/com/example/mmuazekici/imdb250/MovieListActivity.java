96bc9196e1a000617068bcf74388b42fa1e265c5
Mansur Muaz Ekici <mmuazekici@gmail.com>
2018-05-15 10:10:08 +0300

    SharedPreferences handled. Logout Done. Bugs Fixed

--- app/src/main/java/com/example/mmuazekici/imdb250/MovieListActivity.java
+++ app/src/main/java/com/example/mmuazekici/imdb250/MovieListActivity.java
@@ -2,20 +2,28 @@ package com.example.mmuazekici.imdb250;
 
 
 import android.app.LoaderManager;
+import android.content.Context;
 import android.content.CursorLoader;
+import android.content.Intent;
 import android.content.Loader;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.mmuazekici.imdb250.Database.DatabaseConract;
 import com.example.mmuazekici.imdb250.Database.DatabaseHelper;
+import com.example.mmuazekici.imdb250.SignupLogin.LogInActivity;
 
 import java.io.IOException;
 
@@ -40,6 +48,7 @@ public class MovieListActivity extends AppCompatActivity{
         }
 
         RecyclerView rvMovies = findViewById(R.id.rv_movies);
+        TextView usernameTextField = findViewById(R.id.tv_welcome);
 
         LinearLayoutManager layoutManager = new GridLayoutManager(this,1);
         rvMovies.setLayoutManager(layoutManager);
@@ -48,6 +57,15 @@ public class MovieListActivity extends AppCompatActivity{
         mAdapter = new MoviesAdapter(this);
         rvMovies.setAdapter(mAdapter);
 
+        SharedPreferences prefs = getSharedPreferences("pref", MODE_PRIVATE);
+        String username = prefs.getString("username", null);
+        if (username != null){
+            usernameTextField.setText("Welcome " + username);
+        }else{
+            usernameTextField.setText("Welcome");
+        }
+
+
 
         Cursor mCursor = myDbHelper.query(DatabaseConract.MoviesTable.TABLE_NAME, null, null, null, null);
 
@@ -55,7 +73,49 @@ public class MovieListActivity extends AppCompatActivity{
     }
 
 
-//    @Override
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_movie_list, menu);
+
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.userProfile) {
+
+            //TODO: Go to user profile
+
+        } else if (id == R.id.logout) {
+
+            SharedPreferences.Editor editor = getSharedPreferences("pref", MODE_PRIVATE).edit();
+            editor.clear().apply();
+
+
+            Intent startChildActivityIntent = new Intent(this, LogInActivity.class);
+            startActivity(startChildActivityIntent);
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+
+
+    @Override
+    public void onBackPressed() {
+    }
+
+    //    @Override
 //    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
 //        Uri userURI = DatabaseConract.MoviesTable.CONTENT_URI;
 //
--- app/src/main/java/com/example/mmuazekici/imdb250/SignupLogin/LogInActivity.java
+++ app/src/main/java/com/example/mmuazekici/imdb250/SignupLogin/LogInActivity.java
@@ -2,10 +2,12 @@ package com.example.mmuazekici.imdb250.SignupLogin;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -38,46 +40,62 @@ public class LogInActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
 
-        myDbHelper = new DatabaseHelper(this);
 
-        try {
-            myDbHelper.createDataBase();
-        } catch (IOException ioe) {
-            throw new Error("Unable to create database");
-        }
-        try {
-            myDbHelper.openDataBase();
-        } catch (SQLException sqle) {
-            throw sqle;
-        }
+        SharedPreferences prefs = getSharedPreferences("pref", MODE_PRIVATE);
+        Boolean isLoggedIn = prefs.getBoolean("isLoggedIn", false);
+        if ( isLoggedIn){
+            Intent startChildActivityIntent = new Intent(this, MovieListActivity.class);
+            startActivity(startChildActivityIntent);
 
-        logInButton = findViewById(R.id.login_LoginButton);
-        goToSignUpButton = findViewById(R.id.login_goToSignUp);
+        }else{
 
-        usernameTextField = findViewById(R.id.login_usernameTextField);
-        passwordTextField = findViewById(R.id.login_passwordTextField);
+            myDbHelper = new DatabaseHelper(this);
 
-        mContext = getApplicationContext();
-
-        logInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                username = usernameTextField.getText().toString();
-                password = passwordTextField.getText().toString();
-
-                LogInButtonTapped();
+            try {
+                myDbHelper.createDataBase();
+            } catch (IOException ioe) {
+                throw new Error("Unable to create database");
             }
-        });
-
-        goToSignUpButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(LogInActivity.this, SignUpActivity.class));
+            try {
+                myDbHelper.openDataBase();
+            } catch (SQLException sqle) {
+                throw sqle;
             }
-        });
 
+            logInButton = findViewById(R.id.login_LoginButton);
+            goToSignUpButton = findViewById(R.id.login_goToSignUp);
+
+            usernameTextField = findViewById(R.id.login_usernameTextField);
+            passwordTextField = findViewById(R.id.login_passwordTextField);
+
+            mContext = getApplicationContext();
+
+            logInButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    username = usernameTextField.getText().toString();
+                    password = passwordTextField.getText().toString();
+
+                    LogInButtonTapped();
+                }
+            });
+
+            goToSignUpButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(LogInActivity.this, SignUpActivity.class));
+                }
+            });
+
+
+        }
     }
+
+    @Override
+    public void onBackPressed() {
+    }
+
 
 
     public void LogInButtonTapped() {
@@ -101,7 +119,13 @@ public class LogInActivity extends AppCompatActivity {
                     String userID = mCursor.getString(mCursor.getColumnIndex(DatabaseConract.UsersTable.COLUMN_USER_ID));
 
                     Toast.makeText(LogInActivity.this, "Success :) Welcome  " + userName, Toast.LENGTH_SHORT).show();
-                    //TODO: Go to welcome page with ID
+
+
+                    SharedPreferences.Editor editor = getSharedPreferences("pref", MODE_PRIVATE).edit();
+                    editor.putString("username", userName);
+                    editor.putString("userID", userID);
+                    editor.putBoolean("isLoggedIn", true);
+                    editor.apply();
 
                     Intent startChildActivityIntent = new Intent(this, MovieListActivity.class);
                     startChildActivityIntent.putExtra(Intent.EXTRA_UID, userID);
